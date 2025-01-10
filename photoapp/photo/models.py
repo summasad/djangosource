8c31ea23fd5e87ad44977cdbc5dda5c21871964d
김현진 <summersad10@gmail.com>
2025-01-10 18:18:57 +0900

    django

---
+++ photoapp/photo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# 문자열 저장 
+#   CharField(max_length=50) or TextField()
+
+class Photo(models.Model):
+	title = models.CharField(max_length=50)
+	author = models.CharField(max_length=50)
+	image = models.CharField(max_length=50)
+	description = models.TextField()
+	price = models.IntegerField()
+	
+	def __str__(self) :
+	    return self.title
